47411820d02ca26352019a46bc3f53f72e22734d
William Shaw <wilhenryshaw@gmail.com>
2026-01-02 11:54:37 -0500

    Slight tweak to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.tournament_master
 {
    requires javafx.controls;
    requires javafx.fxml;
+   requires javafx.graphics;
    opens app to javafx.fxml;
    opens controllers to javafx.fxml;
-   exports app;
 }
