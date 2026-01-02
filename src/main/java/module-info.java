ed59b56a276dd50194ee9ae5339f6dce2520b91d
William Shaw <wilhenryshaw@gmail.com>
2026-01-02 13:22:43 -0500

    Another slight change to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module com.example.tournament_master
    requires javafx.controls;
    requires javafx.fxml;
    requires javafx.graphics;
-   opens app to javafx.fxml;
+   opens app to javafx.fxml, javafx.graphics;
    opens controllers to javafx.fxml;
 }
